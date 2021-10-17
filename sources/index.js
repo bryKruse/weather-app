77f94613ee2b14d6feedd6cf1647b2a7832accf9
bryKruse <90730678+bryKruse@users.noreply.github.com>
2021-10-17 11:08:23 -0600

    Uploaded weather app
    Index.html: links to Bootstrap, Google fonts, images, css and js files. Basic outline of weather app.
    
    JS file: Current date and time - need to adjust this to work with whatever city/current location user is in. User can type city and JS will show city and current weather. Plus, feels like temp, wind speed, and humidity.
    
    CSS :  current design of weather app and may change slightly in future.

---
+++ sources/index.js
@@ -0,0 +1,120 @@
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let year = now.getFullYear();
+let date = now.getDate();
+
+let dayToday = document.querySelector("#day");
+dayToday.innerHTML = `${day}`;
+let currentDate = document.querySelector("#date");
+currentDate.innerHTML = `${month} ${date}, ${year}`;
+let currentTime = document.querySelector("#time");
+currentTime.innerHTML = now.toLocaleString("en-US", {
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+});
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", chooseCelsius);
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", chooseFahrenheit);
+
+function showTemp(response) {
+  console.log(response);
+  let temperature = Math.round(response.data.main.temp);
+  let cityTemp = document.querySelector("#current-temp");
+  cityTemp.innerHTML = `${temperature}`;
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-day-feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#humid").innerHTML = response.data.main.humidity;
+  document.querySelector("#windy").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather.description;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-city-info");
+  console.log(searchInput.value);
+  let h1 = document.querySelector("#city");
+  let city = searchInput.value;
+  h1.innerHTML = `${searchInput.value}`;
+  let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function findLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector("#current-location");
+button.addEventListener("click", findLocation);
+
+function currentTemp(event) {
+  let cityEl = document.querySelector("#city");
+  let cityinfo = document.querySelector("#search-city-info");
+  cityEl.innerHTML = searchInput.value;
+  cityinfo.innerHTML = searchInput.value;
+  navigator.geolocation.getCurrentPosition(currentTemp);
+  console.log(currentTemp);
+}
+
+let searchForm = document.querySelector("form");
+searchForm.addEventListener("submit", searchCity);
+
+function chooseCelsius(event) {
+  event.preventDefault();
+  let showCelsius = document.querySelector("#current-temp");
+  showCelsius.innerHTML = `25`;
+}
+function chooseFahrenheit(event) {
+  event.preventDefault();
+  let showFahrenheit = document.querySelector("#current-temp");
+  showFahrenheit.innerHTML = `77`;
+}
